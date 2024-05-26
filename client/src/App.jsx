ef2790b57f7305e2cbe6d908219cde1a2662cebd
123ramashish <rashish1349@gmail.com>
2024-05-26 23:53:48 +0530

    images and fake-data add

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,13 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 function App() {
   return (
     <>
-      <h2>Hello</h2>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
