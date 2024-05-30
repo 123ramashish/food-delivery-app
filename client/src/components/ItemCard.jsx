b27bd050bb726243aaa678779a62e649ae638421
123ramashish <rashish1349@gmail.com>
2024-05-30 21:52:38 +0530

    add item card

---
+++ client/src/components/ItemCard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Card } from "flowbite-react";
+import category1 from "../assets/images/category-01.png";
+export default function ItemCard() {
+  return (
+    <Card className="max-w-sm w-fit flex flex-wrap justify-around m-auto my-20 bg-red-200">
+      <div className="mx-12 flex  justify-start ">
+        <div className="flex items-center justify-around gap-8 ">
+          <img src={category1} alt="" />
+          <p className="text-lg">Pizza</p>
+        </div>
+      </div>
+    </Card>
+  );
+}
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import Header from "../components/Header";
 import OrderHome from "../components/OrderHome";
+import ItemCard from "../components/ItemCard";
 
 export default function Home() {
-  return <OrderHome />;
+  return (
+    <>
+      <OrderHome />
+      <ItemCard />
+    </>
+  );
 }
