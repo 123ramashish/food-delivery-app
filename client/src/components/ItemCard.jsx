b4433a522f4144c8cbfec700be16978d4b034cac
123ramashish <rashish1349@gmail.com>
2024-05-30 22:07:31 +0530

    add hover translate effect on card items

--- client/src/components/ItemCard.jsx
+++ client/src/components/ItemCard.jsx
@@ -3,7 +3,7 @@ import { Card } from "flowbite-react";
 import category1 from "../assets/images/category-01.png";
 export default function ItemCard() {
   return (
-    <Card className="max-w-sm w-fit flex flex-wrap justify-around m-auto my-20 bg-red-200">
+    <Card className="max-w-sm w-fit flex flex-wrap justify-around m-auto my-20 bg-red-200 transition delay-150 duration-300 ease-in-out  hover:-translate-y-2">
       <div className="mx-12 flex  justify-start ">
         <div className="flex items-center justify-around gap-8 ">
           <img src={category1} alt="" />
