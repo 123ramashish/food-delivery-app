b59fa44dd4db706bd656ea1a9353c826134024cb
123ramashish <rashish1349@gmail.com>
2024-05-31 19:36:05 +0530

    create delivery features page

--- client/src/components/DeliveryPage.jsx
+++ client/src/components/DeliveryPage.jsx
@@ -1,5 +1,48 @@
 import React from "react";
+import location from "../assets/images/location.png";
+import { CiCircleCheck } from "react-icons/ci";
 
 export default function DeliveryPage() {
-  return <div>DeliveryPage</div>;
+  return (
+    <section className="flex justify-center items-center w-full h-auto px-36 my-8">
+      <div className="">
+        <img src={location} alt="" className="max-h-[700px]" />
+      </div>
+      <div className="left mt-10 ml-20 font-sans dark:text-white">
+        <h1 className="text-bold text-xl font-serif">
+          Why <span className="text-red-600">Tasty Treat?</span>
+        </h1>
+        <p className="text-xs font-thin">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim
+          reiciendis architecto numquam ullam, similique voluptatem.
+        </p>
+        <h5 className=" flex items-center gap-2 text-sm font-serif my-2">
+          <CiCircleCheck className="text-red-600" />
+          Fresh and tasty foods
+        </h5>
+        <p className="text-xs font-thin">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore,
+          aspernatur.
+        </p>
+
+        <h5 className=" flex items-center gap-2 text-sm font-serif my-2">
+          <CiCircleCheck className="text-red-600" />
+          Quality support
+        </h5>
+        <p className="text-xs font-thin">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore,
+          aspernatur.
+        </p>
+
+        <h5 className=" flex items-center gap-2 text-sm font-serif my-2">
+          <CiCircleCheck className="text-red-600" />
+          Order from any location
+        </h5>
+        <p className="text-xs font-thin">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore,
+          aspernatur.
+        </p>
+      </div>
+    </section>
+  );
 }
--- client/src/components/ItemCard.jsx
+++ client/src/components/ItemCard.jsx
@@ -7,7 +7,7 @@ import category4 from "../assets/images/category-04.png";
 export default function ItemCard() {
   return (
     <div className="flex flex-wrap justify-between items-center my-20 ">
-      <Card className="max-w-sm w-fit m-auto mb-8 bg-red-200 transition delay-150 duration-300 ease-in-out  hover:-translate-y-2">
+      <Card className="max-w-sm w-fit m-auto mb-8 bg-red-200 transition delay-150 duration-300 ease-in-out  hover:-translate-y-2 ">
         <div className="mx-12 flex  justify-start ">
           <div className="flex items-center justify-around gap-8 ">
             <img src={category1} alt="" />
