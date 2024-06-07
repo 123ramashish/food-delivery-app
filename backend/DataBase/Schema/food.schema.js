0429c61fb8eff2325900a9e81c34310493661110
123ramashish <rashish1349@gmail.com>
2024-06-07 23:09:05 +0530

    create food schema

---
+++ backend/DataBase/Schema/food.schema.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const FoodSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Food = mongoose.model("Food", FoodSchema);
+
+export default Food;
--- backend/controllers/food.controller.js
+++ backend/controllers/food.controller.js
@@ -1,5 +1,12 @@
 export default class foodController {
   async addFood(req, res, next) {
-    const { name, price } = req.body;
+    const { name, price, imageUrl } = req.body;
+    if (
+      name.trim("") === "" ||
+      price.trim("") === "" ||
+      imageUrl.trim("") === ""
+    ) {
+      return res.status(400).send("Invalid Credentials");
+    }
   }
 }
