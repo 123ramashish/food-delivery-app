6cc1cc5c688c1fa4020d36b6e39d6d89420b99d2
123ramashish <rashish1349@gmail.com>
2024-06-09 15:46:53 +0530

    update cartSlice

--- client/src/Redux/Cart/cartSlice.js
+++ client/src/Redux/Cart/cartSlice.js
@@ -3,6 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   cart: [],
   error: null,
+  totalPrice: 0,
 };
 
 export const cartSlice = createSlice({
@@ -24,9 +25,39 @@ export const cartSlice = createSlice({
     CartFailure: (state, action) => {
       state.error = action.payload;
     },
+    IncreaseQuantity: (state, action) => {
+      const { id } = action.payload;
+      const foundIndex = state.cart.findIndex((item) => item.id === id);
+      if (foundIndex !== -1) {
+        state.cart[foundIndex].quantity += 1;
+      }
+    },
+    DecreaseQuantity: (state, action) => {
+      const { id } = action.payload;
+      const foundIndex = state.cart.findIndex((item) => item.id === id);
+      if (foundIndex !== -1) {
+        if (state.cart[foundIndex].quantity === 1) {
+          state.cart.splice(foundIndex, 1);
+        } else {
+          state.cart[foundIndex].quantity -= 1;
+        }
+      }
+    },
+    CalculateTotalPrice: (state) => {
+      state.totalPrice = state.cart.reduce(
+        (total, item) => total + item.quantity * item.price,
+        0
+      );
+    },
   },
 });
 
-export const { CartSuccess, CartFailure } = cartSlice.actions;
+export const {
+  CartSuccess,
+  CartFailure,
+  IncreaseQuantity,
+  DecreaseQuantity,
+  CalculateTotalPrice,
+} = cartSlice.actions;
 
 export default cartSlice.reducer;
